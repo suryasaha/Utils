18448a02e73885b72adfdc28664553887807c289
Surya Saha <ss2489@cornell.edu>
2014-02-03 16:25:09 -0500

    Removing spl chars before comparison now

--- fasta2fasta.pl
+++ fasta2fasta.pl
@@ -2,13 +2,14 @@
 # MGEL
 # Surya Saha 05/28/04
 # reading fasta file,writing out fasta file, names from given file
-# seq not found to err file
+# seq not found to .notfound file
 
 use strict;
 use warnings;
 
 unless (@ARGV == 3){
 	print "USAGE: $0 <input fasta file>  <destination fasta file> <listfile>\n";
+	print "Can handle special characters in sequence names but will NOT use them for comparison\n";
 	exit;
 }
 
@@ -21,55 +22,66 @@ my $destfname=$ARGV[1];
 chomp ($destfname);
 my $listfname=$ARGV[2];
 chomp ($listfname);
+my $fasta='';
 my $seqname='';
-my $indata='';
 my $haderr=0;
+
+unlink "$destfname.notfound";
+unlink $destfname;
 
 unless(open(OUTFILEDATA,">$destfname")){print "not able to open ".$destfname."\n\n";exit;}
 unless(open(LISTFILEDATA,$listfname)){print "not able to open ".$listfname."\n\n";exit;}
 
 while($seqname=<LISTFILEDATA>){# for each seq name in listfile
-	#$seqname=~ s/\s*//; #removing white space at end
-	chomp $seqname;
+	my $rawseqname=$seqname;
+	#print STDERR "Before trimming list ".$seqname;
+	$seqname=~ s/\s*//g; #removing white space
+	$seqname =~ s/\W*//g; #removing non alphanumeric or _ , non-word
+	#print STDERR "Post trimming list ".$seqname."\n";	
 	unless(open(INFILEDATA,$srcfname)){print "not able to open ".$srcfname."\n\n";exit;}
 	my $found=0;
 	my $flag=0;
-	while ($indata=<INFILEDATA>){# input file loop
-		#chomp $indata;
-		my $tmpdata=$indata;
-		#$tmpdata=~ s/\s*//; #removing white space 
-		if($flag==0 && ($tmpdata=~ /$seqname/)){# match seq name with > included
-		#if($flag==0 && ($indata eq ">".$seqname)){# match seq name with > included
-			print OUTFILEDATA $indata;
-			$flag=1; #set flag for next loop
-			$found=1;
+	while ($fasta=<INFILEDATA>){# input fasta loop
+		#chomp $fasta;
+		my $tmpdata=$fasta;
+		
+		if (($flag==0) && ($tmpdata =~ /^>/)){
+			#print STDERR "Before trimming ".$tmpdata;
+			$tmpdata =~ s/\s*//g; #removing white space
+			$tmpdata =~ s/\W*//g;
+			#print STDERR "Post trimming ".$tmpdata."\n";
+
+			if($flag==0 && ($tmpdata eq $seqname)){# match seq name with > included
+				print OUTFILEDATA $fasta;
+				$flag=1; #set flag for next loop
+				$found++;
+				print STDERR "Found ".$rawseqname;
+			}
+		}
+		elsif ($flag==1 and $found==1 and ! ($fasta=~ />/)){
+			print OUTFILEDATA $fasta;
 			next;
 		}
-		elsif ($flag==1 and $found==1 and ! ($indata=~ />/)){
-			print OUTFILEDATA $indata;
-			next;
-		}
-		elsif ($flag==1 and $found==1 and $indata=~ />/){
-			#$flag=0; #reset flag for next sequence in list file
+		elsif ($flag==1 and $found==1 and $fasta=~ />/){
 			goto OUT;
 		}
 	}
 	OUT:
 	close INFILEDATA;
-	
 	if ($found==0){
 		unless(open(ERRFILEDATA,">>$destfname.notfound"))
 			{print "not able to open ".$destfname."notfound\n\n";exit;}
-		print ERRFILEDATA $seqname;
-		#print $seqname." not found!!\n";
+		print ERRFILEDATA $seqname."\n";
+		print "Cannot find ".$rawseqname;
 		close ERRFILEDATA;
 		$haderr=1;
 	}
 }
 
 if ($haderr==1){
-	print "\nsequences not found are in ".$destfname.".notfound file\n";
+	print "\nSequences not found are in ".$destfname.".notfound file\n";
 }
+
 
 close OUTFILEDATA;
 close LISTFILEDATA;
