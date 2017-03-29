e08157268c8cc9b1c1a515c69b901a1a3f05fe63
Surya Saha <ss2489@cornell.edu>
2017-03-29 11:06:22 -0400

    using sensible var names

--- find_RBH_orthologs.sh
+++ find_RBH_orthologs.sh
@@ -10,9 +10,12 @@
 #gi|662182964|ref|XP_008487638.1| FBpp0291925
 #gi|662182972|ref|XP_008470659.1| FBpp0311427
 
+# NOTE 
+# NOT TESTED
+
 usage(){
 	echo "usage:
-	$0 <GI or accession list> <80perc orthologs> <60perc orthologs> <50perc orthologs>
+	$0 <GI or accession list> <90perc orthologs> <80perc orthologs> <70perc orthologs>"
 	exit 1
 }
 
@@ -21,23 +24,26 @@ then
 	usage
 fi
 
-printf "ARG 1 : %s \n" "$1"
-printf "ARG 2 : %s \n" "$2"
-printf "ARG 3 : %s \n" "$3"
-printf "ARG 4 : %s \n" "$4"
+printf "GI or accession list : %s \n" "$1"
+printf "90perc orthologs : %s \n" "$2"
+printf "80perc orthologs : %s \n" "$3"
+printf "70perc orthologs : %s \n" "$4"
 
+LIST="$1"
+PERC90="$2"
+PERC80="$3"
+PERC70="$4"
 
-grep -f "$1" "$2" > "${1}"."${2}".orthologs
+grep -f "$LIST" "$PERC90" > "${1}"."${2}".orthologs
 
-awk -F"|" '{print $2}' "${1}"."${2}".orthologs | grep -f - -v "$1"| grep -f - "$3" > "${1}"."${3}".orthologs
+awk -F"|" '{print $PERC90}' "${1}"."${2}".orthologs | grep -f - -v "$LIST"| grep -f - "$PERC80" > "${1}"."${PERC80}".orthologs
 
-awk -F"|" '{print $2}' <(cat "${1}"."${2}".orthologs "${1}"."${3}".orthologs) | grep -f - -v "$1"| grep -f - "$4" > "${1}"."${4}".orthologs
+awk -F"|" '{print $PERC90}' <(cat "${1}"."${2}".orthologs "${1}"."${PERC80}".orthologs) | grep -f - -v "$LIST"| grep -f - "$4" > "${1}"."${PERC70}".orthologs
 
-awk -F"|" '{print $2}' <(cat "${1}"."${2}".orthologs "${1}"."${3}".orthologs "${1}"."${4}".orthologs) | grep -f - -v "$1"| grep -f - "$5" > "${1}"."${5}".orthologs
+awk -F"|" '{print $PERC90}' <(cat "${1}"."${2}".orthologs "${1}"."${PERC80}".orthologs "${1}"."${PERC70}".orthologs) | grep -f - -v "$LIST"| grep -f - "$5" > "${1}"."${5}".orthologs
 
-awk -F"|" '{print $2}' <(cat "${1}"."${2}".orthologs "${1}"."${3}".orthologs "${1}"."${4}".orthologs "${1}"."${5}".orthologs) | grep -f - -v "$1"| grep -f - "$" > "${1}"."${6}".orthologs
+awk -F"|" '{print $PERC90}' <(cat "${1}"."${2}".orthologs "${1}"."${PERC80}".orthologs "${1}"."${PERC70}".orthologs "${1}"."${5}".orthologs) | grep -f - -v "$LIST"| grep -f - "$" > "${1}"."${6}".orthologs
 
-awk -F"|" '{print $2}' <(cat "${1}"."${2}".orthologs "${1}"."${3}".orthologs "${1}"."${4}".orthologs "${1}"."${5}".orthologs "${1}"."${6}".orthologs) | grep -f - -v "$1"| grep -f - "$" > "${1}"."${7}".orthologs
+awk -F"|" '{print $PERC90}' <(cat "${1}"."${2}".orthologs "${1}"."${PERC80}".orthologs "${1}"."${PERC70}".orthologs "${1}"."${5}".orthologs "${1}"."${6}".orthologs) | grep -f - -v "$LIST"| grep -f - "$" > "${1}"."${7}".orthologs
 
-awk -F"|" '{print $2}' <(cat "${1}"."${2}".orthologs "${1}"."${3}".orthologs "${1}"."${4}".orthologs "${1}"."${5}".orthologs "${1}"."${6}".orthologs "${1}"."${7}".orthologs) | grep -f - -v "$1"| grep -f - "$" > "${1}"."${8}".orthologs
-
+awk -F"|" '{print $PERC90}' <(cat "${1}"."${2}".orthologs "${1}"."${PERC80}".orthologs "${1}"."${PERC70}".orthologs "${1}"."${5}".orthologs "${1}"."${6}".orthologs "${1}"."${7}".orthologs) | grep -f - -v "$LIST"| grep -f - "$" > "${1}"."${8}".orthologs
