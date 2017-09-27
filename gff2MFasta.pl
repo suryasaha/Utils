5a21a25cbe075a34c92f0a5b92e05cecef7d3a2d
Surya Saha <ss2489@cornell.edu>
2017-09-27 10:44:32 -0400

    now works for fasta and gffwith multiple seqs

--- gff2MFasta.pl
+++ gff2MFasta.pl
@@ -27,10 +27,10 @@ use Getopt::Long;
  Command-line options can be abbreviated to single-letter options, e.g. -f instead of --file. Some options
 are mandatory (see below).
 
-   --fna    <.fna>  ONLY ONE sequence in fasta format (required)
-   --gff    <.gff>  GFF file (required)
-   --minlen <500>   Minimum length of illuminated region, if screening
-   --out    <.fna>  Name of output mfasta file
+   --fna    <.fna>  Sequences in fasta format (required)
+   --gff    <.gff>  GFF file, Seqid required in .fna file (required)
+   --minlen <500>   Minimum length of  region
+   --out    <.fas>  Name of output mfasta file
 
 =head1 AUTHOR
 
@@ -38,7 +38,7 @@ are mandatory (see below).
 
 =cut
 
-my($fna,$gff,$scr,$out,$mlen,@maskedseq,$seq,$i,$j,$rec,@temp,$ctr,%illumreg);
+my($fna,$gff,$out,$mlen,$seq,$seqname,$i,$j,$rec,@temp,$ctr,$notfoundctr,%seqs);
 
 GetOptions (
 	'fna=s' => \$fna,
@@ -60,35 +60,61 @@ unless(open(OUT,">$out")){print "not able to open $out\n\n";exit 1;}
 # read in files
 $seq='';
 while($rec=<INFAS>){
-	if($rec =~ />/){next;}
+	if($rec =~ /^>/){
+		if ($seq ne '' && $seqname ne ''){
+			#record last seq and reset
+			$seqs{$seqname}=$seq; $seq='';
+		}
+
+		#get seq name till first space
+		if ($rec =~ / /){#if it has space
+			@temp = split (' ',$rec); $seqname= $temp[0];
+		}
+		else{
+			$seqname= $rec; chomp $seqname;
+		}
+		$seqname=~ s/\>//;
+		next;
+	}
 	$rec=~ s/\s*//g;#clean
 	$seq=$seq.$rec;
 }
+#last seq
+$seqs{$seqname}=$seq;
 close(INFAS);
 
 #gff_seqname	artemis	exon	12136	12720	.	+	.	ID=CDS:12136..12720
 #gff_seqname	artemis	exon	69917	70358	.	+	.	ID=CDS:69917..70358
 #gff_seqname	artemis	exon	71700	72604	.	+	.	ID=CDS:71700..72604
 # read in files
-$ctr=0;
+$ctr=0;$notfoundctr=0;
 while($rec=<INGFF>){
 	if($rec =~ /#/){next;}
 	@temp=split("\t",$rec);
+	
+	if (!exists $seqs{$temp[0]}){ #if feature not found
+		print STDERR "$temp[0] not found in $fna for GFF record $rec";
+		$notfoundctr++;
+		next;
+	}
 
 	if($temp[8] ne ''){
-		chomp $temp[8]; 
-		print OUT "\>$temp[8]\:pos $temp[3]-$temp[4] \:length ",$temp[4]-$temp[3]+1,"\n";
+		chomp $temp[8];
+		my @attr = split ("\;",$temp[8]);
+		print OUT "\>$temp[0]\:$attr[0]\:$temp[3]-$temp[4]\:length ",$temp[4]-$temp[3]+1,"\n";
 	}
 	else{ print OUT "\>$ctr\n"}
+	
 	if(($temp[6] eq '+') || ($temp[6] eq '.')){
-		print OUT substr($seq,$temp[3]-1,(($temp[4]-1)-($temp[3]-1)+1)),"\n";#coordinate space to index space
+		print OUT substr($seqs{$temp[0]},$temp[3]-1,(($temp[4]-1)-($temp[3]-1)+1)),"\n";#coordinate space to index space
 	}
 	elsif($temp[6] eq '-'){
-		print OUT &SS::revcomp(substr($seq,$temp[3]-1,(($temp[4]-1)-($temp[3]-1)+1))),"\n";#coordinate space to index space
+		print OUT &SS::revcomp(substr($seqs{$temp[0]},$temp[3]-1,(($temp[4]-1)-($temp[3]-1)+1))),"\n";#coordinate space to index space
 	}
 	$ctr++;		
 }
 close(INGFF);
-print STDERR "GFF should only have features on ONE DNA sequence. $ctr records processed\n";
+print STDERR "$ctr records processed.\n";
+if ($notfoundctr > 0){ print STDERR "$notfoundctr records not found. Please check GFF and Fasta file\n";}
 
 exit;
