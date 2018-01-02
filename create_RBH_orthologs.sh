d9cfdcc8aeedcb5f252e66e3a588c6ff7d7aeca6
Surya Saha <ss2489@cornell.edu>
2018-01-02 11:49:45 -0500

    removed loop. writing manually.

--- create_RBH_orthologs.sh
+++ create_RBH_orthologs.sh
@@ -33,19 +33,89 @@ printf "ARG 4 : %s \n" "$4"
 
 
 # create blast reports with query and hit lists
-arr_blast=(90 80 70 60 50 40 30 20 10 5); 
+#arr_blast=(90 80 70 60 50); 
 
-for N in "${arr_blast[@]}"; 	do 
-	echo "$N"; BlastReport.pl -r "$1" --qcutoff "$N" --scutoff "$N" --besthit 1 --qhits 1 --out "$1".scov"${N}".qcov"${N}".xls;
-	done
+#for N in "${arr_blast[@]}"; 	do 
+#	echo "$N"; BlastReport.pl -r "$1" --qcutoff "$N" --scutoff "$N" --besthit 1 --qhits 1 --out "$1".scov"${N}".qcov"${N}".xls;
+#done
 
-for N in "${arr_blast[@]}"; do 
-	echo "$N"; BlastReport.pl -r "$2" --qcutoff "$N" --scutoff "$N" --besthit 1 --qhits 1 --out "$2".scov"${N}".qcov"${N}".xls; 
-	done
+#for N in "${arr_blast[@]}"; do 
+#	echo "$N"; BlastReport.pl -r "$2" --qcutoff "$N" --scutoff "$N" --besthit 1 --qhits 1 --out "$2".scov"${N}".qcov"${N}".xls; 
+#	done
 
 # create ortholog sets
-arr_ortho=(80 60 50 40 20 10 5); 
-for N in "${arr_ortho[@]}"; do 
-		join -1 1 -2 2 <$(paste "$1".scov"${N}".qcov"${N}".xls.querywthit.names "$1".scov"${N}".qcov"${N}".xls.querywthit_besthit.names| sort) <$(paste "$2".scov"${N}".qcov"${N}".xls.querywthit.names "$2".scov"${N}".qcov"${N}".xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}' > "$3"-"$FOUR"_"${N}"perc_scovqcov.orthologs
-	done
+#arr_ortho=(80 60 50 40 20 10 5); 
+#for N in "${arr_ortho[@]}"; do 
+#		join -1 1 -2 2 <$(paste "$1".scov"${N}".qcov"${N}".xls.querywthit.names "$1".scov"${N}".qcov"${N}".xls.querywthit_besthit.names| sort) <$(paste "$2".scov"${N}".qcov"${N}".xls.querywthit.names "$2".scov"${N}".qcov"${N}".xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}' > "$3"-"$FOUR"_"${N}"perc_scovqcov.orthologs
+#	done
 
+#printf "\n\n90 percent orthologs\n\n"
+#REMOVE ALL REDIRECTS. USE TEMP FILES
+
+#DATA=`join -1 1 -2 2 <$(paste "$1".scov90.qcov90.xls.querywthit.names "$1".scov90.qcov90.xls.querywthit_besthit.names| sort) <$(paste "$2".scov90.qcov90.xls.querywthit.names "$2".scov90.qcov90.xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}'`
+#paste "$1".scov95.qcov95.xls.querywthit.names "$1".scov95.qcov95.xls.querywthit_besthit.names| sort > temp1
+#paste "$2".scov95.qcov95.xls.querywthit.names "$2".scov95.qcov95.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+#Not checking order as join does not understand non-string sort and gene ids are in version
+#DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+#DATA=`join -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+#unlink temp1
+#unlink temp2
+#echo "$DATA" > "$3"-"$4".95.orthologs
+#wc -l "$3"-"$4".95.orthologs
+
+
+paste "$1".scov90.qcov90.xls.querywthit.names "$1".scov90.qcov90.xls.querywthit_besthit.names| sort > temp1
+paste "$2".scov90.qcov90.xls.querywthit.names "$2".scov90.qcov90.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+unlink temp1
+unlink temp2
+echo "$DATA" > "$3"-"$4".90.orthologs
+wc -l "$3"-"$4".90.orthologs
+
+paste "$1".scov80.qcov80.xls.querywthit.names "$1".scov80.qcov80.xls.querywthit_besthit.names| sort > temp1
+paste "$2".scov80.qcov80.xls.querywthit.names "$2".scov80.qcov80.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+unlink temp1
+unlink temp2
+echo "$DATA" > "$3"-"$4".80.orthologs
+wc -l "$3"-"$4".80.orthologs
+
+paste "$1".scov70.qcov70.xls.querywthit.names "$1".scov70.qcov70.xls.querywthit_besthit.names| sort > temp1
+paste "$2".scov70.qcov70.xls.querywthit.names "$2".scov70.qcov70.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+unlink temp1
+unlink temp2
+echo "$DATA" > "$3"-"$4".70.orthologs
+wc -l "$3"-"$4".70.orthologs
+
+paste "$1".scov60.qcov60.xls.querywthit.names "$1".scov60.qcov60.xls.querywthit_besthit.names| sort > temp1
+paste "$2".scov60.qcov60.xls.querywthit.names "$2".scov60.qcov60.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+unlink temp1
+unlink temp2
+echo "$DATA" > "$3"-"$4".60.orthologs
+wc -l "$3"-"$4".60.orthologs
+
+paste "$1".scov50.qcov50.xls.querywthit.names "$1".scov50.qcov50.xls.querywthit_besthit.names| sort > temp1
+paste "$2".scov50.qcov50.xls.querywthit.names "$2".scov50.qcov50.xls.querywthit_besthit.names| sort -k 2,2 > temp2
+DATA=`join --nocheck-order -1 1 -2 2 temp1 temp2| awk '$2==$3'| awk '{print $1,$2}'`
+unlink temp1
+unlink temp2
+echo "$DATA" > "$3"-"$4".50.orthologs
+wc -l "$3"-"$4".50.orthologs
+
+#printf "\n\n80 percent orthologs\n\n"
+#DATA=`join -1 1 -2 2 <$(paste "$1".scov80.qcov80.xls.querywthit.names "$1".scov80.qcov80.xls.querywthit_besthit.names| sort) <$(paste "$2".scov80.qcov80.xls.querywthit.names "$2".scov80.qcov80.xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}'`
+#printf "$DATA"
+
+#printf "\n\n70 percent orthologs\n\n"
+#DATA=`join -1 1 -2 2 <$(paste "$1".scov70.qcov70.xls.querywthit.names "$1".scov70.qcov70.xls.querywthit_besthit.names| sort) <$(paste "$2".scov70.qcov70.xls.querywthit.names "$2".scov70.qcov70.xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}'`
+#printf $DATA
+
+#printf "\n\n60 percent orthologs\n\n"
+#DATA=`join -1 1 -2 2 <$(paste "$1".scov60.qcov60.xls.querywthit.names "$1".scov60.qcov60.xls.querywthit_besthit.names| sort) <$(paste "$2".scov60.qcov60.xls.querywthit.names "$2".scov60.qcov60.xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}'`
+#printf $DATA
+
+#printf "\n\n50 percent orthologs\n\n"
+#DATA=`join -1 1 -2 2 <$(paste "$1".scov50.qcov50.xls.querywthit.names "$1".scov50.qcov50.xls.querywthit_besthit.names| sort) <$(paste "$2".scov50.qcov50.xls.querywthit.names "$2".scov50.qcov50.xls.querywthit_besthit.names| sort -k 2,2)| awk '$2==$3'| awk '{print $1,$2}'`
+#printf $DATA
