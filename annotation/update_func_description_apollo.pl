4f33dddf0104591e2488a05acd286593a2bbf32a
Surya Saha <ss2489@cornell.edu>
2019-10-07 08:24:34 -0400

    added commnets, fixed vars, implemented -d

--- annotation/update_func_description_apollo.pl
+++ annotation/update_func_description_apollo.pl
@@ -54,7 +54,7 @@ my @lines          = split( /\n/, $input_curated );
 foreach my $line (@lines) {
 	chomp($line);
 	my @line_arr = split ("\t", $line);
-	$apollo_curated_function{$line_arr[0]}=$line_arr[1];
+	$apollo_curated_function{$line_arr[0]}=$line_arr[1];            #apollo id = curated function
 }
 
 my %OGS_maker_index;
@@ -62,7 +62,7 @@ my %OGS_maker_index;
 foreach my $line (@lines) {
 	chomp($line);
 	my @line_arr = split ("\t", $line);
-	$OGS_maker_index{$line_arr[1]}=$line_arr[0];
+	$OGS_maker_index{$line_arr[1]}=$line_arr[0];                    # OGS id = maker is
 }
 
 my %OGS_ahrd_function;
@@ -70,7 +70,7 @@ my %OGS_ahrd_function;
 foreach my $line (@lines) {
 	chomp($line);
 	my @line_arr = split ("\t", $line);
-	$OGS_ahrd_function{$line_arr[0]}=$line_arr[1];
+	$OGS_ahrd_function{$line_arr[0]}=$line_arr[1];                  # OGS id = AHRD function string
 }
 
 my ($OGS_id, $ahrd_function, $output_data, $curated_count );
@@ -79,9 +79,11 @@ $curated_count = 0;
 
 while (($OGS_id, $ahrd_function) = each %OGS_ahrd_function){
 	if ( ! exists $OGS_maker_index{$OGS_id} ) { die "each OGS id will have a maker id\n"; }
-	my $maker_id = $OGS_maker_index{$OGS_id};
+	my $maker_apollo_id = $OGS_maker_index{$OGS_id};
 	my $suffix; #-RA,-RB,-RC.... 1-16 in this dataset
 	my @OGS_arr = split ('\.', $OGS_id);
+
+	#get isoform number (1 in Dcitr00g00990.1.1, 2 in Dcitr00g01090.1.2)
 	if ( $OGS_arr[2] == 1 ){ $suffix = '-RA'; }
 	elsif ( $OGS_arr[2] == 2 ){ $suffix = '-RB'; }
 	elsif ( $OGS_arr[2] == 3 ){ $suffix = '-RC'; }
@@ -100,25 +102,38 @@ while (($OGS_id, $ahrd_function) = each %OGS_ahrd_function){
 	elsif ( $OGS_arr[2] == 16 ){ $suffix = '-RP'; }
 
 	#add in curated function if present
-	if ( exists $apollo_curated_function{$maker_id} ){
-		#print STDERR "$maker_id\n";
+	if ( exists $apollo_curated_function{$maker_apollo_id} ){
+
+		if ($opt_d){
+			print STDERR "$maker_apollo_id\n";
+		}
+
 		$curated_count++;
-		my $curated_function = $apollo_curated_function{$maker_id};
+		my $curated_function = $apollo_curated_function{$maker_apollo_id};
 
-		#print STDERR "Found curated function $curated_function for $OGS_id with maker id $maker_id\n";
-
+		if ($opt_d){
+			print STDERR "Found curated function $curated_function for $OGS_id with maker id $maker_apollo_id\n";
+		}
+		
 		my $updated_function = $curated_function.$suffix.'. ';
 		my $new_ahrd_function;
 
 		#AHRD
 		#Polyprotein (AHRD V3.11 *-* tr|A0A0L7L4A5|A0A0L7L4A5_9NEOP). Similar to MCOT21850.0.CC. AED 0.01 => Manual desc (AHRD V3.11 *-* Polyprotein tr|A0A0L7L4A5|A0A0L7L4A5_9NEOP). Similar to MCOT21850.0.CC. AED 0.01
 		if ( $ahrd_function =~ /AHRD/ ){
-			#print STDERR "Auto: $ahrd_function\n";
+
+			if ($opt_d){
+				print STDERR "Auto: $ahrd_function\n";
+			}
+
 			my @temp_arr = split (' \(', $ahrd_function);
 			my $AHRD_function = $temp_arr[0]; #ugly!!
 			#$AHRD_function =~ s/^[\S\s]+ \(/\(/;
 			#$AHRD_function =~ s/ \($//;
-			#print STDERR "AHRD: $AHRD_function\n";
+
+			if ($opt_d){
+				print STDERR "AHRD: $AHRD_function\n";
+			}
 
 			$ahrd_function =~ s/^[\S\s]+ \(/\(/;
 			my @ahrd_function_arr = split (' ', $ahrd_function);
@@ -145,9 +160,13 @@ while (($OGS_id, $ahrd_function) = each %OGS_ahrd_function){
 			die "this should not have happened!!\n";
 		}
 		$updated_function = $updated_function. $new_ahrd_function;
+		
 		#print to out
 		$output_data = $output_data.$OGS_id."\t".$updated_function."\n";
-		#print STDERR "Updated: $updated_function\n";
+
+		if ($opt_d){
+			print STDERR "Updated: $updated_function\n";
+		}
 	}
 	else{#no curated funciton
 	#print to out
