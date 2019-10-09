1dcc43b17e95ad91232b2720f3c87b389c367364
Surya Saha <ss2489@cornell.edu>
2019-10-09 09:41:19 -0400

    clened comments

--- annotation/add_OGSids_GFF.pl
+++ annotation/add_OGSids_GFF.pl
@@ -23,7 +23,7 @@ my %mRNA_index_hash;
 my @lines = split (/\n/, $input_mRNA_index);
 foreach my $line (@lines){
 	my @arr = split (/\t/, $line);
-	$mRNA_index_hash{$arr[0]} = $arr[1];
+	$mRNA_index_hash{$arr[0]} = $arr[1];								#snap-DC3.0sc00-processed-gene-3.9-mRNA-1	Dcitr00g01000.1.1
 }
 
 my @gene_arr;
@@ -50,22 +50,20 @@ foreach my $line (@lines){
 			}			
 		}
 		if (!exists $mRNA_index_hash{$mRNA_id}) { die "$mRNA_id not found in hash"; }
-		#fix ID and Name
-		$line =~ s/${mRNA_id}/$mRNA_index_hash{$mRNA_id}/g;
+		
+		$line =~ s/${mRNA_id}/$mRNA_index_hash{$mRNA_id}/g;					#fix ID and Name
 		my $parent = $mRNA_index_hash{$mRNA_id};
 		$parent =~ s/\.\d+$//;
-		#fix parent
-		$line =~ s/Parent\=\S+\;/Parent=$parent\;/;
 		
-		#print gene record
-		for my $val (0..7){
+		$line =~ s/Parent\=\S+\;/Parent=$parent\;/;							#fix parent
+		
+		for my $val (0..7){													#print gene record
 			print OUTGFF $gene_arr[$val]."\t";
 		}
 		print OUTGFF 'ID='.$parent.';Name='.$parent."\n";
 		#@gene_arr = ();
 		
-		#print mRNA record
-		print OUTGFF $line."\n";
+		print OUTGFF $line."\n";											#print mRNA record
 		
 		print STDERR "$parent done..\n"
 	}
@@ -73,17 +71,16 @@ foreach my $line (@lines){
 		#fix Name and Parent
 		$line =~ s/${mRNA_id}/$mRNA_index_hash{$mRNA_id}/g;
 
-		#multi parent child
-		if ( $line =~ m/,/ ){
+		if ( $line =~ m/,/ ){												#multi parent child
 			my @arr = split (/\t/, $line);
 			my @attrib_arr = split (/\;/, $arr[8]);
 			foreach my $attrib (@attrib_arr){
 				if ( $attrib =~ /^Parent\=/ ){
 					$attrib =~ s/^Parent\=//;
 					my @parents = split ( /,/, $attrib);
+
 					foreach my $parent (@parents){
-						#do if maker/apollo id
-						if ( $parent !=~ /^Dcitr/ ){
+						if ( $parent !~ /^Dcitr/ ){							#do if not Dcitr id, so only for maker/apollo id
 							if (!exists $mRNA_index_hash{$parent}) { die "$parent parent not found in hash"; }
 							$line =~ s/${parent}/$mRNA_index_hash{$parent}/;
 						}						
@@ -91,14 +88,9 @@ foreach my $line (@lines){
 				}			
 			}
 		}
-
-
 		print OUTGFF $line."\n";
 	}
 }
-
-
-
 
 close (OUTGFF);
 
